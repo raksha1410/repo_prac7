f7a6cb43bd00d8d75eb6a0a617196c64596227aa
raksha1410 <rakshithah1499@gmail.com>
2023-08-05 22:18:22 +0530

    shell script for soft link added 2

---
+++ soft_link_prac2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Replace "soft_link.txt" with the name of your soft link
+soft_link="soft_link.txt"
+
+# Replace "target_file.txt" with the name of the target file for the soft link
+target_file="target_file.txt"
+
+# Check if the soft link exists and is valid
+if [ -L "$soft_link" ] && [ -e "$soft_link" ]; then
+          echo "The soft link '$soft_link' points to: $(readlink -f "$soft_link")"
+          # Perform any operations with the target file here
+  else
+          echo "The soft link '$soft_link' is missing or broken."
+          # Check if the target file exists
+          if [ -e "$target_file" ]; then
+                  # Create the soft link
+                  ln -s "$target_file" "$soft_link"
+                  echo "Soft link '$soft_link' has been created and now points to '$target_file'."
+          else
+                  echo "Unable to create the soft link as the target file '$target_file' is missing."
+          fi
+fi
